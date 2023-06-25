979033b78c895004e8986636ad6d7ff9ff2cfd4a
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-06-24 22:01:02 -0500

    crud listo

--- src/pages/apoderados/Parentesco.jsx
+++ src/pages/apoderados/Parentesco.jsx
@@ -19,6 +19,15 @@ function Parentesco(){
         })
     },[bandera])
 
+    function mostrar(cod){
+        console.log("mostrando codigo " + cod)
+        axios.get(apiAurl+cod)
+        .then(res=>{
+            setId(res.data.parentesco_id)
+            setNombre(res.data.parentesco_nombre)
+        })
+    }
+
     function guardar(e){
         e.preventDefault()
         let cod = id
@@ -27,6 +36,16 @@ function Parentesco(){
         }
         if(cod > 0){
             //actualizar
+            console.log(datos)
+            axios.put(apiAurl+cod+'/',datos)
+            .then(res=>{
+                console.log(res.data)
+                setNombre('')
+                setId(null)
+                setBandera(true)
+            }).catch((error)=>{
+                console.log(error.toString())
+            })
         }
         else{
             axios.post(apiAurl,datos)
@@ -41,6 +60,17 @@ function Parentesco(){
         }
     }
 
+    function eliminar(cod){
+        let rpta = window.confirm("desea eliminar")
+        if (rpta){
+            axios.delete(apiAurl+cod+'/')
+            .then(res=>{
+                console.log(res.data)
+                setBandera(true)
+            })
+        }
+    }
+
     return(
         <AdminLayout>
             <>
@@ -50,7 +80,7 @@ function Parentesco(){
                     <label className="form-label">Nuevo Parentesco</label>
                     <input type="hidden" value={id}/>
                     <input type="text" className="form-control" 
-                    id="nombre" 
+                    id="nombre" value={nombre}
                     onChange={(e)=>setNombre(e.target.value)}
                     />
                     <br/>
@@ -72,8 +102,14 @@ function Parentesco(){
                                 <th scope="row" key={par.id}>{par.parentesco_id}</th>
                                 <td>{par.parentesco_nombre}</td>
                                 <td>
-                                    <button type="button" className="btn btn-primary">Editar</button>
-                                    <button type="button" className="btn btn-danger">Eliminar</button>
+                                    <button type="button" 
+                                    className="btn btn-primary"
+                                    onClick={()=>mostrar(par.parentesco_id)}
+                                    >Editar</button>
+                                    <button type="button"
+                                    className="btn btn-danger"
+                                    onClick={()=>eliminar(par.parentesco_id)}
+                                    >Eliminar</button>
                                 </td>
                             </tr>
                         )
